6a990ba9e91f9c8ac9886f25bed583363ead03fd
Eli Doran <eli@elidoran.com>
2017-05-19 11:13:48 -0400

    rename inputs for ordering and length, fix array2

--- benchmark/json/inputs/6_array2-long.js
+++ benchmark/json/inputs/6_array2-long.js
@@ -1,1 +1,1 @@
-module.exports = '[ 1, [ 2, [ 3, 4 ], 5 ], "string", 60.04e-1, "7.0", "object" : { "abc": "def", "array": [ "xyz", 789 ], "ghi" : 789 }, 0, "done" ]'
+module.exports = '[ 1, [ 2, [ 3, 4 ], 5 ], "string", 60.04e-1, "7.0", { "some" : "object" , "abc": "def", "array": [ "xyz", 789 ], "ghi" : 789 }, 0, "done" ]'
